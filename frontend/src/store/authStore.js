df9906aeaabc2d294e1e02e6287dd32f28b988c6
lenlo121500 <raulc8808@gmail.com>
2025-05-15 14:03:21 +0800

    fix: typo in the authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api/auth"
-    : "/api.auth";
+    : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
